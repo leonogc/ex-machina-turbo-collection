8db737141b5e43a441a195cac3476f56a1439f4d
leonogc <37965130+leonogc@users.noreply.github.com>
2022-06-26 18:33:21 -0300

    test: Testing Formidable

--- src/Controllers/filesController.js
+++ src/Controllers/filesController.js
@@ -9,23 +9,31 @@ const pathVideos = path.join(__dirname, "../temp/videos");
 
 module.exports = {
     uploadImage : async(req, res) => {
-        await new formidable.IncomingForm().parse(req, async(err, fields, files) => {
-            if(err){
-                throw err;
-            }
-            console.log("files", files);
-            const rawData = fs.readFileSync(files.image.filepath);
-            id = uuid4();
-            imagePath = path.join(pathImages, id + ".png");
-            await fs.writeFile(imagePath, rawData, async(err) => {
-                if(err) {
-                    console.log(err);
-                    res.status(400).send({status: "Failed"});
-                }
+        let image;
+        var form = new formidable.IncomingForm();
+        form.parse(req,function(err,fields,files){
+            console.log(fields, files)
+            postData = fields;
+            image = postData.image;
+        });
+        res.send("DOne")
+        // await new formidable.IncomingForm().parse(req, async(err, fields, files) => {
+        //     if(err){
+        //         throw err;
+        //     }
+        //     console.log("files", files);
+        //     const rawData = fs.readFileSync(files.image.filepath);
+        //     id = uuid4();
+        //     imagePath = path.join(pathImages, id + ".png");
+        //     await fs.writeFile(imagePath, rawData, async(err) => {
+        //         if(err) {
+        //             console.log(err);
+        //             res.status(400).send({status: "Failed"});
+        //         }
 
-                res.status(200).send({status: "Done", id, path: `${id}.png`});
-            });
-        })
+        //         res.status(200).send({status: "Done", id, path: `${id}.png`});
+        //     });
+        // })
     },
 
     uploadVideo : async(req, res) => {
