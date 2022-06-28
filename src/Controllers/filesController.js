28decbe9186d834bcaecfa5f7b378ff1bf9889df
leonogc <37965130+leonogc@users.noreply.github.com>
2022-06-27 22:38:33 -0300

    feat: added fallback :)

--- src/Controllers/filesController.js
+++ src/Controllers/filesController.js
@@ -10,21 +10,28 @@ const pathVideos = path.join(__dirname, "../temp/videos");
 module.exports = {
     uploadImage : async(req, res) => {
         await new formidable.IncomingForm().parse(req, async(err, fields, files) => {
+            console.log(fields)
             console.log(files);
             if(err){
                 throw err;
             }
-            const rawData = fs.readFileSync(files.image.filepath);
-            id = uuid4();
-            imagePath = path.join(pathImages, id + ".png");
-            await fs.writeFile(imagePath, rawData, async(err) => {
-                if(err) {
-                    console.log(err);
-                    res.status(400).send({status: "Failed"});
-                }
-
-                res.status(200).send({status: "Done", id, path: `${id}.png`});
-            });
+            try{
+                const rawData = fs.readFileSync(files.image.filepath);
+                id = uuid4();
+                imagePath = path.join(pathImages, id + ".png");
+                await fs.writeFile(imagePath, rawData, async(err) => {
+                    if(err) {
+                        console.log(err);
+                        res.status(400).send({status: "Failed"});
+                    }
+    
+                    res.status(200).send({status: "Done", id, path: `${id}.png`});
+                });
+            }
+            catch(err){
+                console.log(err)
+                res.status(400).send({status: "Failed"});
+            }
         })
     },
 
@@ -34,17 +41,23 @@ module.exports = {
             if(err){
                 throw err;
             }
-            const rawData = fs.readFileSync(files.video.filepath);
-            id = uuid4();
-            videoPath = path.join(pathVideos, id + ".mp4");
-            await fs.writeFile(videoPath, rawData, async(err) => {
-                if(err) {
-                    console.log(err);
-                    res.status(400).send({status: "Failed"});
-                }
-
-                res.status(200).send({status: "Done", id, path: `${id}.mp4`});
-            });
+            try{
+                const rawData = fs.readFileSync(files.video.filepath);
+                id = uuid4();
+                videoPath = path.join(pathVideos, id + ".mp4");
+                await fs.writeFile(videoPath, rawData, async(err) => {
+                    if(err) {
+                        console.log(err);
+                        res.status(400).send({status: "Failed"});
+                    }
+    
+                    res.status(200).send({status: "Done", id, path: `${id}.mp4`});
+                });
+            }
+            catch(err){
+                console.log(err)
+                res.status(400).send({status: "Failed"});
+            }
         })
     },
 }
